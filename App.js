994a8c1d46459f2776d10146764b62d7ad876f91
Asselya1999 <bersinbayeva@gmail.com>
2022-05-18 16:15:49 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,116 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeStackNavigator, ContactStackNavigator } from "./StackNavigator";
+import { Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SplashScreen from './screens/SplashScreen';
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import DrawerNavigationRoutes from "./screens/DrawerNavigationRoutes";
+const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+const Auth = () => {
+  return (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={RegisterScreen}
+        options={{
+          title: "Register",
+          headerStyle: {
+            backgroundColor: "#307ecc",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeStackNavigator}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/icons/home-outline-svg.png")}
+              style={{ width: 20, height: 20 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={ContactStackNavigator}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/icons/people-outline-svg.png")}
+              style={{ width: 20, height: 20 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Authorization"
+        component={StackContainerScreen}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("./assets/icons/register.webp")}
+              style={{ width: 20, height: 20 }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+function StackContainerScreen() {
+  return (
+    <Stack.Navigator initialRouteName="SplashScreen">
+      <Stack.Screen 
+        name="SplashScreen"
+        component={SplashScreen}
+        // Hiding header for Splash Screen
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Auth"
+        component={Auth}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DrawerNavigationRoutes"
+        component={DrawerNavigationRoutes}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <BottomTabNavigator />
+      </NavigationContainer>
+    );
+  }
+}
